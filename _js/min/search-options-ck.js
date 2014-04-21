6cd3b9904d7f8684aa921ca84a85e98fbd139d75
thejamesdempsey <hello@thejamesdempsey.com>
2014-04-21 16:32:49 -0400

    Search component

---
+++ _js/min/search-options-ck.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".js-search-menu").click("slider__reverse"),$(".js-search-menu").on("click",function(e){$(body).toggleClass("menu-active"),$("js-primary-search").toggleClass("active"),e.preventDefault()})});
