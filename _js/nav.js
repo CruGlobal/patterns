23e4f257513cafcdb1e38a2596f5f7076edb9a7d
Richard Coy <richard.coy@cru.org>
2014-05-29 14:17:38 -0400

    Added hover/click js

--- _js/nav.js
+++ _js/nav.js
@@ -1,3 +1,29 @@
 $(".site--select").change(function() {
   window.location = $(this).find("option:selected").val();
 });
+
+/*
+* Detect Browser Width
+*/
+
+// On Page Load
+$(window).ready(function() {
+    var wi = $(window).width();  
+    if (wi <= 980){
+            $( ".nav" ).removeClass( ".dropdown" ).addClass( ".off-canvas-desk" );
+            }
+        else {
+            $( ".nav" ).removeClass( ".off-canvas-desk" ).addClass( ".dropdown" );
+            }
+    // On Window Resize
+    $(window).resize(function() {
+        var wi = $(window).width();
+
+        if (wi <= 980){
+            $( ".nav" ).removeClass( ".dropdown" ).addClass( ".off-canvas-desk" );
+            }
+        else {
+            $( ".nav" ).removeClass( ".off-canvas-desk" ).addClass( ".dropdown" );
+            }
+    });            
+});
